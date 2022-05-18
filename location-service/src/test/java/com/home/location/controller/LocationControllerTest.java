41567cbd75ef5f2f87a4436a15e30d27c742f8cf
siarhei.hrynkou <siarhei.hrynkou@autodesk.com>
2022-05-18 21:38:02 +0300

    Training ticket - added tests to location-service

---
+++ location-service/src/test/java/com/home/location/controller/LocationControllerTest.java
@@ -0,0 +1,94 @@
+package com.home.location.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.home.location.domain.Location;
+import com.home.location.dto.LocationDto;
+import com.home.location.repository.LocationRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestPropertySource(properties = {"spring.config.location = classpath:application-integrationtest.yml"})
+class LocationControllerTest {
+    private static final String requestMapping = "/locations";
+    private static final String postMapping = "/";
+    private static final String getMapping = "/";
+    @Autowired
+    private ObjectMapper objectMapper;
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private LocationRepository repository;
+
+    @Test
+    void saveLocation() throws Exception {
+        Long locationId = null;
+        String country = "test_country";
+        String city = "test_city";
+        LocationDto locationDto = createLocationDto(locationId, country, city);
+
+        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders
+                        .post(requestMapping + postMapping)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(locationDto)))
+                .andDo(print());
+
+        resultActions.andExpect(MockMvcResultMatchers.status().isOk());
+        LocationDto resultLocationDto = objectMapper.readValue(
+                resultActions.andReturn().getResponse().getContentAsString(), LocationDto.class);
+        assertEquals(city, resultLocationDto.getLocationCity());
+        assertEquals(country, resultLocationDto.getLocationCountry());
+        assertNotEquals(locationId, resultLocationDto.getId());
+    }
+
+    @Test
+    void findLocationById() throws Exception {
+        Long locationId = null;
+        String country = "test_country";
+        String city = "test_city";
+        Location location = createLocation(locationId, country, city);
+        locationId = repository.save(location).getId();
+
+        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders
+                        .get(requestMapping + getMapping + locationId)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print());
+
+        resultActions.andExpect(MockMvcResultMatchers.status().isOk());
+        LocationDto resultLocationDto = objectMapper.readValue(
+                resultActions.andReturn().getResponse().getContentAsString(), LocationDto.class);
+        assertEquals(city, resultLocationDto.getLocationCity());
+        assertEquals(country, resultLocationDto.getLocationCountry());
+        assertEquals(locationId, resultLocationDto.getId());
+    }
+
+    private Location createLocation(Long locationId, String country, String city) {
+        Location location = new Location();
+        location.setId(locationId);
+        location.setLocationCountry(country);
+        location.setLocationCity(city);
+        return location;
+    }
+
+    private LocationDto createLocationDto(Long locationId, String country, String city) {
+        LocationDto locationDto = new LocationDto();
+        locationDto.setId(locationId);
+        locationDto.setLocationCountry(country);
+        locationDto.setLocationCity(city);
+        return locationDto;
+    }
+}
---
+++ location-service/src/test/java/com/home/location/repository/LocationRepositoryTest.java
@@ -0,0 +1,65 @@
+package com.home.location.repository;
+
+import com.home.location.domain.Location;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@TestPropertySource(properties = {"spring.config.location = classpath:application-test.yml"})
+class LocationRepositoryTest {
+
+    @Autowired
+    private LocationRepository locationRepository;
+
+    @AfterEach
+    void clearDatabase() {
+        locationRepository.deleteAll();
+    }
+
+    @Test
+    void locationRepository_all_are_ok_save() {
+        Long locationId = null;
+        String country = "country";
+        String city = "city";
+        Location locationToSave = createLocation(locationId, country, city);
+
+        Location location = locationRepository.save(locationToSave);
+
+        assertNotEquals(locationId, location.getId());
+        assertEquals(country, location.getLocationCountry());
+        assertEquals(city, location.getLocationCity());
+    }
+
+    @Test
+    void locationRepository_all_are_ok_findById() {
+        Long locationId = null;
+        String country = "country";
+        String city = "city";
+        Location location = createLocation(locationId, country, city);
+        locationId = locationRepository.save(location).getId();
+
+        Optional<Location> optionalLocation = locationRepository.findById(locationId);
+
+        assertTrue(optionalLocation.isPresent());
+        Location result = optionalLocation.get();
+        assertEquals(locationId, result.getId());
+        assertEquals(country, result.getLocationCountry());
+        assertEquals(city, result.getLocationCity());
+    }
+
+    private Location createLocation(Long locationId, String country, String city) {
+        Location location = new Location();
+        location.setId(locationId);
+        location.setLocationCountry(country);
+        location.setLocationCity(city);
+        return location;
+    }
+}
